2a1e3b78329fcc06b6475d3422011e5830bf0301
Naveen Sanjaya Basnayake <108771214+NaveenSanjaya@users.noreply.github.com>
2025-07-09 23:44:15 +0530

    Delete spring-boot-crud-rest-api-main/src/main/java/com/msd/spring_boot_rest_api/repository/OrderRepository.java

--- spring-boot-crud-rest-api-main/src/main/java/com/msd/spring_boot_rest_api/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.msd.spring_boot_rest_api.repository;
-
-import com.msd.spring_boot_rest_api.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
